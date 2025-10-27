51e8481de4155860c8bd08e53707b9708f5cf419
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2025-10-27 10:37:20 -0500

    Proyecto incial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('alojha');
+  protected readonly footer = signal('Universidad del Quindío 2025');
 }
