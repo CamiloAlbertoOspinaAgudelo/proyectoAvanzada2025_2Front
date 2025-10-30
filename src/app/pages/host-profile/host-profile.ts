6a8253ea27f051923688a2a9cbb3ebb24a6a2c02
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2025-10-30 09:17:54 -0500

    alojha

---
+++ src/app/pages/host-profile/host-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host-profile',
+  imports: [],
+  templateUrl: './host-profile.html',
+  styleUrl: './host-profile.css'
+})
+export class HostProfile {
+
+}
---
+++ src/app/pages/owned-accommodations/owned-accommodations.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owned-accommodations',
+  imports: [],
+  templateUrl: './owned-accommodations.html',
+  styleUrl: './owned-accommodations.css'
+})
+export class OwnedAccommodations {
+
+}
