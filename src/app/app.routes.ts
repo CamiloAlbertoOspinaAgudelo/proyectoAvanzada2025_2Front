48cc6bc3463132798073269d043271c0c6a7062c
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2025-11-01 12:25:59 -0500

    createPlace route no sirve

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
-//import { CreatePlace } from './pages/create-place/create-place';
+import { CreateAccommodation } from './pages/create-accommodation/create-accommodation';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
-    { path: "**", pathMatch: "full", redirectTo: "" }
+    { path: "**", pathMatch: "full", redirectTo: "" },
+    {path: 'create-accommodation', component: CreateAccommodation}
 ];
