56ae0a54e6544ca6c549543ab031fb458f987b91
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2025-11-12 18:22:23 -0500

    Mensaje

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,19 +11,22 @@ import { Accommodation } from './pages/accommodation/accommodation';
 import { EditPlace } from './pages/edit-place/edit-place';
 import { UserProfile } from './pages/user-profile/user-profile';
 import { loginGuard } from './services/login-service';
+import { CreateBooking } from './pages/create-booking/create-booking';
+import { roleGuard } from './guards/role-service';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
-    { path: 'create-accommodation', component: CreateAccommodation },
+    { path: 'create-accommodation', component: CreateAccommodation, canActivate: [roleGuard], data: { expectedRole: ["ROLE_HOST"] } },
     { path: 'edit-user', component: EditUser },
     { path: 'recover-password', component: RecoverPassword },
     { path: 'reset-password', component: ResetPassword },
-    { path: 'my-places', component: MyPlaces },
+    { path: 'my-places', component: MyPlaces, canActivate: [roleGuard], data: { expectedRole: ["ROLE_HOST"] } },
     { path: 'accommodation/:id', component: Accommodation },
-    { path: 'edit-place/:id', component: EditPlace },
+    { path: 'edit-place/:id', component: EditPlace, canActivate: [roleGuard], data: { expectedRole: ["ROLE_HOST"] } },
     { path: 'user-profile', component: UserProfile },
+    { path: 'create-booking', component: CreateBooking, canActivate: [roleGuard], data: { expectedRole: ["ROLE_USER"] } },
     { path: 'login', component: Login, canActivate: [loginGuard] },
     { path: 'register', component: Register, canActivate: [loginGuard] },
     { path: "**", pathMatch: "full", redirectTo: "" }
